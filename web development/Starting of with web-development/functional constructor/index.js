23ade7d78db5cbdfdb75a57132ccd3f9a277b32a
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-02-17 19:16:49 +0530

    class

--- web development/Starting of with web-development/functional constructor/index.js
+++ web development/Starting of with web-development/functional constructor/index.js
@@ -17,3 +17,22 @@ Person.prototype.major = function () {
 const p1 = new Person("ram", "kumar", "M", 23);
 console.log(p1.major());
 p1.getFullName();
+
+//ES6 class
+
+class Person1 {
+  constructor(name, lastname, sex, age) {
+    this.name = name;
+    this.lastname = lastname;
+    this.age = age;
+    this.sex = sex;
+  }
+
+  getFullName() {
+    console.log(`${this.name} ${this.lastname}`);
+  }
+}
+
+const p2 = new Person1("ram", "kumar", "M", 23);
+// console.log(p1.major());
+p2.getFullName();
