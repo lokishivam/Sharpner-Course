9545ef8faf87ae6e7d6c272eeac73310c95f8558
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-03-14 12:21:47 +0530

    updates, using app.get and app.post

--- 2. Backend Development with Nodejs/2. Starting with Express Framework/2. intro to middlewares/app.js
+++ 2. Backend Development with Nodejs/2. Starting with Express Framework/2. intro to middlewares/app.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 //we need to add a body-parser to parse the body of the incoming data.
 //we will use a middleware to do so.
 app.use(bodyParser.urlencoded({ extended: false }));
-//now the req.body is parsed into from form into object
+//now the req.body is parsed from form into object
 // in plain nodejs we had to use buffer to gather data uing a eventlistner.
+//it will also call the next method
 
 app.use("/product", (req, res, next) => {
   console.log("welcome to middleware product");
@@ -19,14 +20,16 @@ app.use("/product", (req, res, next) => {
 </form>`);
 });
 
-app.use("/product-detail", (req, res, next) => {
+app.post("/product-detail", (req, res, next) => {
   console.log("welcome to product-details middleware ");
   console.log(req.body);
   //redirect to '/'
   res.redirect("/"); // in plain nodejs we had to chance the headers of the response object.
 });
 
-app.use("/", (req, res, next) => {
+//app.get does the same thing as app.use. It also cheacks wether the req is get or not
+//It also does a exact match i.e. localhost:3000/help wont be matched by app.get('/', ..)
+app.get("/", (req, res, next) => {
   console.log("welcome to middleware ");
   res.send("<h1> hello to node js </h1>");
 });
