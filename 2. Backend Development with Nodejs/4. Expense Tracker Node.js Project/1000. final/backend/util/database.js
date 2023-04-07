c37e43ead8f96a85bae57abd3cb9bda186a955aa
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-04-08 02:16:26 +0530

    backend ready

--- 2. Backend Development with Nodejs/4. Expense Tracker Node.js Project/1000. final/backend/util/database.js
+++ 2. Backend Development with Nodejs/4. Expense Tracker Node.js Project/1000. final/backend/util/database.js
@@ -1,10 +1,16 @@
 //Sequelize is a class
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
 //sequelize object will used to create connections and manage
-const sequelize = new Sequelize("expense-app-sharpner", "root", "johncena48", {
-  dialect: "mysql", //sequelize is also used for many other databases, so we specify
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql", //sequelize is also used for many other databases, so we specify
+    host: process.env.DB_HOST,
+  }
+);
 
 module.exports = sequelize;
