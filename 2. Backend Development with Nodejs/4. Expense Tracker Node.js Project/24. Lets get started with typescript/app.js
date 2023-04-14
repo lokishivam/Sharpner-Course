197ba6a576c803233f76a0200dbb199cc37ed85d
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-04-14 18:09:19 +0530

    type script learning

--- 2. Backend Development with Nodejs/4. Expense Tracker Node.js Project/24. Lets get started with typescript/app.js
+++ 2. Backend Development with Nodejs/4. Expense Tracker Node.js Project/24. Lets get started with typescript/app.js
@@ -1,11 +1,12 @@
+"use strict";
 //you get a general HTMLElemt as type because you are getting element using id.
-var num1Element = document.getElementById("number1"); //type-casting
-var num2Element = document.getElementById("number2");
-var buttonElement = document.querySelector("button"); //the HTMLButtonElement can be null, if no button exist so !
+const num1Element = document.getElementById("number1"); //type-casting
+const num2Element = document.getElementById("number2");
+const buttonElement = document.querySelector("button"); //the HTMLButtonElement can be null, if no button exist so !
 // let num3 = 10; //you dont need to specify the type here, ts will detect it.
 // num3 = 'ram'; //error shown by ts
-var numArray = [];
-var textArray = [];
+const numArray = [];
+const textArray = [];
 function addition(num1, num2) {
     //uninon
     if (typeof num1 === "number" && typeof num2 === "number") {
@@ -19,11 +20,11 @@ function printResult(resultObj) {
     //way to specify the type of the object
     console.log(resultObj.val);
 }
-buttonElement.addEventListener("click", function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value; //the value will be string
-    var result = addition(+num1, +num2);
-    var stringRes = addition(num1, num2); //string inputs
+buttonElement.addEventListener("click", () => {
+    let num1 = num1Element.value;
+    let num2 = num2Element.value; //the value will be string
+    const result = addition(+num1, +num2);
+    const stringRes = addition(num1, num2); //string inputs
     //const stringBool = addition(true, false);
     console.log(result);
     numArray.push(result);
