c4d62a32a12b8f273fa371442352998906eecbb2
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-04-29 05:04:53 +0530

    learning mongoDB

---
+++ 2. Backend Development with Nodejs/6.Placement Module/models/product.js
@@ -0,0 +1,25 @@
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save() {
+    //save the object to the mongoDB collection i.e Product
+    const db = getDb();
+    db.collections("product")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = Product;
---
+++ 2. Backend Development with Nodejs/6.Placement Module/util/database.js
@@ -0,0 +1,27 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+exports.mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://lokishivam:d9bBDVURk9wzQFTG@cluster0.atzztfh.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("connected");
+      _db = client.db();
+      //once a connection is established, the client object is used to access databases and
+      //collections within the instance.
+      //The client.db() method is used to access a database from the connected MongoDB instance.
+      callback();
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getDb = () => {
+  if (_db) {
+    return db;
+    //the client object is used to access databases and collections within the instance.
+  }
+  throw new Error("no connection found");
+};
