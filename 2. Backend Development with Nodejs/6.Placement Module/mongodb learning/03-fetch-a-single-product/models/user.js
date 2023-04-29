99c5aa0614364d1e6392f6352c4f36b18475bd18
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-04-29 21:41:09 +0530

    userTable

--- 2. Backend Development with Nodejs/6.Placement Module/mongodb learning/03-fetch-a-single-product/models/user.js
+++ 2. Backend Development with Nodejs/6.Placement Module/mongodb learning/03-fetch-a-single-product/models/user.js
@@ -1,16 +1,45 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    db.collection("users")
+      .inserOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  static findById(userId) {
+    const db = getDb();
+    db.collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+// const User = sequelize.define('user', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   name: Sequelize.STRING,
+//   email: Sequelize.STRING
+// });
 
 module.exports = User;
