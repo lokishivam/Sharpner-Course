8d6438ef2641118b478ccbbf6aa82116739037be
lokishivam <94531871+lokishivam@users.noreply.github.com>
2023-04-29 20:53:00 +0530

    Integrating CRUD with MongoDB

--- 2. Backend Development with Nodejs/6.Placement Module/mongodb learning/03-fetch-a-single-product/models/product.js
+++ 2. Backend Development with Nodejs/6.Placement Module/mongodb learning/03-fetch-a-single-product/models/product.js
@@ -1,5 +1,5 @@
-const mongodb = require('mongodb');
-const getDb = require('../util/database').getDb;
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -9,15 +9,21 @@ class Product {
     this.imageUrl = imageUrl;
   }
 
-  save() {
+  save(id) {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
-      .then(result => {
-        console.log(result);
+    let dbOp;
+    if (id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(id) }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
+      .then((result) => {
+        //console.log(result);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
@@ -25,14 +31,14 @@ class Product {
   static fetchAll() {
     const db = getDb();
     return db
-      .collection('products')
+      .collection("products")
       .find()
       .toArray()
-      .then(products => {
+      .then((products) => {
         console.log(products);
         return products;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
@@ -40,14 +46,27 @@ class Product {
   static findById(prodId) {
     const db = getDb();
     return db
-      .collection('products')
+      .collection("products")
       .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
-      .then(product => {
+      .then((product) => {
         console.log(product);
         return product;
       })
-      .catch(err => {
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("deleted");
+      })
+      .catch((err) => {
         console.log(err);
       });
   }
